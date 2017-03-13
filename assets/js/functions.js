eef6cad46a59bbaa6fc030f199dcdf10793e2740
dnore <dexter.nore@live.com>
2017-03-14 11:53:45 +1300

    Post
    Post Description

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,4 +1,26 @@
 $( document ).ready(function() {
 
+  $(window).scroll(function() {
+
+  	//Activates when scrolling up and down
+  	var wScroll = $(this).scrollTop();
+
+  	if(wScroll > $('.newish').offset().top - $(window).height()) {
+
+  		var offset = Math.min(0, wScroll - $('.newish').offset().top + $(window).height() - 900);
+
+  			$('.post1').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 0.5) +'px)'});
+  			$('.post2').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.5) +'px)'});
+  			$('.post3').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 0.5) +'px)'});
+  			$('.post4').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.5) +'px)'});
+  			$('.post5').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 0.5) +'px)'});
+  			$('.post6').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.5) +'px)'});
+  			$('.post7').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 0.5) +'px)'});
+  			$('.post8').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.5) +'px)'});
+  	}
+
+
+
+  });
 
 });
