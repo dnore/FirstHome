a90e55f1d0401dd90103afdd129d0f1a3e467435
dnore <dexter.nore@live.com>
2017-03-14 13:16:12 +1300

    done
    done

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,23 +2,25 @@ $( document ).ready(function() {
 
   $(window).scroll(function() {
 
-  	//Activates when scrolling up and down
-  	var wScroll = $(this).scrollTop();
+    if (document.documentElement.clientWidth > 640 ) {
+    	//Activates when scrolling up and down
+    	var wScroll = $(this).scrollTop();
 
-  	if(wScroll > $('.newish').offset().top - $(window).height()) {
+    	if(wScroll > $('.newish').offset().top - $(window).height()) {
 
-  		var offset = Math.min(0, wScroll - $('.newish').offset().top + $(window).height() - 600);
+    		var offset = Math.min(0, wScroll - $('.newish').offset().top + $(window).height() - 800);
 
-  			$('.post1').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 0.5) +'px)'});
-  			$('.post2').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.5) +'px)'});
-  			$('.post3').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 0.5) +'px)'});
-  			$('.post4').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.5) +'px)'});
-  			$('.post5').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 0.5) +'px)'});
-  			$('.post6').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.5) +'px)'});
-  			$('.post7').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 0.5) +'px)'});
-  			$('.post8').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.5) +'px)'});
-  	}
+    			$('.post1').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 0.5) +'px)'});
+    			$('.post2').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.5) +'px)'});
+    			$('.post3').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 0.5) +'px)'});
+    			$('.post4').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.5) +'px)'});
+    			$('.post5').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 0.5) +'px)'});
+    			$('.post6').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.5) +'px)'});
+    			$('.post7').css({'transform': 'translate('+ offset +'px, '+ Math.abs(offset * 0.5) +'px)'});
+    			$('.post8').css({'transform': 'translate('+ Math.abs(offset) +'px, '+ Math.abs(offset * 0.5) +'px)'});
 
+      }
+    }
   });
 
 });
